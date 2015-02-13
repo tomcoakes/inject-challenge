88fe5f7b30d837a7a3d34f8cb91568a2326bdb27
Tom Coakes <tomcoakes@gmail.com>
2015-02-13 15:39:48 +0000

    Basic functionality of inject works (passing in blocks and initial values)

--- lib/injector.rb
+++ lib/injector.rb
@@ -1,7 +1,15 @@
 class Array
 
-  def injector(initial_value = self[0])
-    block_given? ? yield : initial_value
+  def injector(initial_value = 0)
+    if block_given?
+      self.each do |element|
+        initial_value = yield(initial_value, element)
+      end
+      initial_value
+    else
+      initial_value = self[0] if initial_value == 0
+      initial_value
+    end
   end
 
 end
--- spec/injector_spec.rb
+++ spec/injector_spec.rb
@@ -3,13 +3,10 @@ require 'injector'
 describe Array do
   describe "Array#injector" do
 
-  # should be able to yield to a block
-  # should accept an optional 'initial number' argument
-  # should use the first element of the array as the initial value of accum if no 'initial number' argument is passed
-  # should accept a method argument (as a symbol)
   # should return the accumulator argument
   # should return 1 when calling on [1] with the block {|accum, element| accum + element}
   # should return 15 when calling on [1, 2, 3, 4, 5] with the block {|accum, element| accum + element}
+  # should accept a method argument (as a symbol)
 
     it "can take a block as an argument" do
       expect([1].injector{"this is a block"}).to eq("this is a block")
@@ -25,6 +22,18 @@ describe Array do
 
     it "will use the value at index[0] when not explicitly passed an initial value" do
       expect([1].injector).to eq(1)
+    end
+
+    it "returns 3 when called on [1, 2]" do
+      expect([1, 2].injector{|accum, element| accum + element}).to eq(3)
+    end
+
+    it "returns 15 when called on [1, 2, 3, 4, 5]" do
+      expect([1, 2, 3, 4, 5].injector{|accum, element| accum + element}).to eq(15)
+    end
+
+    it "returns 20 when called on [1, 2, 3, 4, 5] with an initial value of 5" do
+      expect([1, 2, 3, 4, 5].injector(5) {|accum, element| accum + element}).to eq(20)
     end
 
   end
