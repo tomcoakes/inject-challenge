52559eada10e23ae3bdd1d73b461de6511aa97f0
Tom Coakes <tomcoakes@gmail.com>
2015-02-16 22:07:13 +0000

    Update injector.rb

--- lib/injector.rb
+++ lib/injector.rb
@@ -3,10 +3,12 @@ class Array
   def injector(accum = 0)
     return nil if self.empty?
     if block_given?
-      self.each {|element| accum = yield(accum, element)}; accum
+      self.each {|element| accum = yield(accum, element)}
+      accum
     elsif accum.is_a?(Symbol)
       result = 0
-      self.each {|element| result = result.send(accum, element)}; result
+      self.each {|element| result = result.send(accum, element)}
+      result
     else
       accum = self.first if accum == 0; accum
     end
